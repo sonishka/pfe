48fdde7c2d14f98b957205ee159dcdeb419929a1
unknown <soniahassouna1@gmail.com>
2018-02-11 19:26:52 +0000

    first commit

---
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lenovo.myapplication;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
